f6b2a486637185f1a1fcd409ac83187b6a89bd05
Rupa Gadewar <rupa@gadewar.com>
2023-05-23 19:12:49 -0500

    Improved logic

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment code here
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -7,58 +6,34 @@ function writePassword()
 {  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  // if password value has trithy value only then we show
-
+  // if password value has truthy value only then we show
   if(password){
   passwordText.value = password;
   }
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword()
- {
-  passwordLength = createPasswordOptions();
+{
+  var passwordLength = prompt('How many characters would you like your password to contain (Must be 8 -128 caharacters) and must be valid number');
+  // check = passwordLength >= 8 && passwordLength <= 128 && !isNaN(passwordLength);
+  if (!passwordLength) {
+    alert("Please enter valid length");
+    return;
+  }
+  else{
+  passwordLength = createPasswordOptions(passwordLength);}
   // next prompts are taken only when password length is valid
   if(passwordLength)
     {
-    specialCharChoice = confirm("Click OK to confirm including special characters");
-    console.log("Special Char Choice: " + specialCharChoice);
-    numericChoice = confirm("Click OK to confirm including numeric characters ");
-    console.log("numericChoice: " + numericChoice);
-    upperCaseChoice = confirm("Click OK to confirm including upper characters ");
-    console.log("upperCaseChoice: " + upperCaseChoice);
-    lowerCaseChoice = confirm("Click OK to confirm including lower characters ");
-    console.log("lowerCaseChoice: " + lowerCaseChoice);
-    
-    // Show this prompt only if no character type is selected
-    if (!specialCharChoice && !numericChoice && !upperCaseChoice && !lowerCaseChoice)
-    {
-    var letterCase = prompt("Please enter case for password (ex. lower, upper, both, numeric, special):", "enter case");
-    // Make sure to convert all choices to lowercase for comparision oLowerCase() is used only if user eneter some case
-    if(letterCase !== null)
-    {
-    letterCase = letterCase.trim();
-    letterCase = letterCase.toLowerCase();
-    }
-    var check = letterCase !== "lower" && letterCase !== "upper" && letterCase !=="both" && letterCase !== null && !specialCharChoice && !numericChoice 
-     && letterCase !=="numeric" && letterCase !=="special" && !upperCaseChoice && !lowerCaseChoice;
-    while(check ||letterCase === null)
-    {
-      alert("please enter a valid case");
-      var letterCase = prompt("Please enter case for apssword (ex. lower, upper, both, numeric, special):", "enter case");
-      letterCase = letterCase.trim();
-      if(letterCase !== null)
-      {
-      letterCase = letterCase.trim();
-      letterCase = letterCase.toLowerCase();
-      }
-      check = letterCase !== "lower" && letterCase !== "upper" && letterCase !=="both" && letterCase !== null && !specialCharChoice && !numericChoice
-      && letterCase !=="numeric" && letterCase !=="special" && !upperCaseChoice && !lowerCaseChoice;
-    } 
-    console.log("Case: " + letterCase);
-  }
+    var charChoiceFunc = charInput();
+    console.log("charChoiceFunc");
+    [specialCharChoice, numericChoice, upperCaseChoice, lowerCaseChoice]
+    specialCharChoice = charChoiceFunc[0];
+    numericChoice = charChoiceFunc[1];
+    upperCaseChoice = charChoiceFunc[2];
+    lowerCaseChoice = charChoiceFunc[3]
   }
   // creating password string for randomization
   var characters = '';
@@ -73,25 +48,22 @@ function generatePassword()
    if(passwordLength !== undefined)
     {
     // If user choose specialCharChoice yes, concat '~!@#%^&*(){}-_+=?' to characters
-    if(specialCharChoice || letterCase === 'special'){
+    if(specialCharChoice){
       characters += specialChars;
     }
     //If user choose numericChoice yes, concat '0123456789' to characters
-    if(numericChoice || letterCase === 'numeric'){
+    if(numericChoice){
       characters += numericChars;
     }
-    if(letterCase === 'lower' || lowerCaseChoice )
-    {
-      characters += lowerCaseChars;
-    }
-    if (letterCase === 'upper' || upperCaseChoice)
+    if (upperCaseChoice)
     {
       characters += upperCaseChars;
     }
-    if (letterCase === 'both')
-      characters += bothCase;
+    if(lowerCaseChoice )
+    {
+      characters += lowerCaseChars;
     }
-
+  }
   const charactersLength = characters.length;
   // Randomization to character string
   for ( let i = 0; i < passwordLength; i++ ) {
@@ -100,16 +72,9 @@ function generatePassword()
   return result;
 }
 
-function createPasswordOptions(){
-
-  var passwordLength = prompt('How many characters would you like your password to contain (Must be 8 -128 caharacters) and must be valid number');
-  // check = passwordLength >= 8 && passwordLength <= 128 && !isNaN(passwordLength);
-  if (!passwordLength) {
-    alert("Please enter valid length");
-    return;
-  }
+function createPasswordOptions(passwordLength)
+{
   check = passwordLength >= 8 && passwordLength <= 128;
-
   while(!check)
   {
     alert("Please try again, password length must be 8 -128 characters and must be valid number");
@@ -119,5 +84,19 @@ function createPasswordOptions(){
     return passwordLength;
 }
 
-
-
+function charInput(){
+    specialCharChoice = confirm("Click OK to confirm including special characters");
+    console.log("Special Char Choice: " + specialCharChoice);
+    numericChoice = confirm("Click OK to confirm including numeric characters ");
+    console.log("numericChoice: " + numericChoice);
+    upperCaseChoice = confirm("Click OK to confirm including upper characters ");
+    console.log("upperCaseChoice: " + upperCaseChoice);
+    lowerCaseChoice = confirm("Click OK to confirm including lower characters ");
+    console.log("lowerCaseChoice: " + lowerCaseChoice);
+    if (!specialCharChoice && !numericChoice && !upperCaseChoice && !lowerCaseChoice)
+    { 
+      alert("Please enter atleast one character type");
+      charInput(); 
+    }
+    return  [specialCharChoice, numericChoice, upperCaseChoice, lowerCaseChoice];
+}
